27856ce709ec87bb90250b8570e8d3d05f178cde
Statham <ferdesonyason@gmail.com>
2018-03-02 17:43:43 +0500

    feat(html, js, scss): add slider, but not done yet

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import './app.scss';
 import { initIcons } from './icons';
+import { lory } from 'lory.js';
+
+
+const slider = document.querySelector('.js_slider');
+
+lory(slider, {
+  infinite: 1,
+});
+
 
 initIcons();
