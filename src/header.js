a8f11c4888bf7ae94e72dbefd2fc2a48729e7ee5
Sergey Klevakin <justerest@yandex.ru>
2018-03-02 22:01:23 +0500

    feat(header): add toggle nav logic

--- src/header.js
+++ src/header.js
@@ -1,16 +1,27 @@
 onScroll(window);
 window.addEventListener('scroll', onScroll);
 
-const menuIconEl = document.querySelector('.menu-icon');
-menuIconEl.addEventListener('click', () => {
-  if (menuIconEl.classList.contains('menu-icon_opened')) {
-    menuIconEl.classList.remove('menu-icon_opened');
-  }
-  else menuIconEl.classList.add('menu-icon_opened');
-});
+document.querySelector('.menu-icon').addEventListener('click', () => toggleNav());
 
 function onScroll(event) {
   const headerEl = document.querySelector('.header');
   if (event.pageY > 150) headerEl.classList.add('header_position_not-top');
-  else headerEl.classList.remove('header_position_not-top');
+  else {
+    headerEl.classList.remove('header_position_not-top');
+    toggleNav(true);
+  }
+}
+
+function toggleNav(isClose = false) {
+  const menuIconEl = document.querySelector('.menu-icon');
+  const navEl = document.querySelector('.nav');
+
+  if (menuIconEl.classList.contains('menu-icon_opened') || isClose) {
+    menuIconEl.classList.remove('menu-icon_opened');
+    navEl.classList.remove('nav_opened');
+  }
+  else {
+    menuIconEl.classList.add('menu-icon_opened');
+    navEl.classList.add('nav_opened');
+  }
 }
